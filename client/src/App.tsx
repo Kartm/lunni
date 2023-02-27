3c24efa02d43abd680e1f54e7be2e8743696b693
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-02-27 21:01:12 +0100

    Add scaffolding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Router, Routes} from "react-router-dom";
+import {HomePage} from "./pages/home";
+import {MergerPage} from "./pages/merger";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <nav>
+              <ul>
+                  <li>
+                      <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link to="/merger">Merger</Link>
+                  </li>
+              </ul>
+          </nav>
+
+          <Routes>
+              <Route path="/merger" element={<MergerPage />} />
+              <Route path="/" element={<HomePage />} />
+          </Routes>
+      </div>
   );
 }
 
---
+++ client/src/pages/merger/MergerPage.tsx
@@ -0,0 +1,3 @@
+export const MergerPage = () => {
+    return <div>merger</div>
+}
