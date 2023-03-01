a6ed19195d8c389533ad2de6b3bfa261e2be0974
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-03-01 16:04:25 +0100

    Add id to plain transaction model

--- server/merger/models.py
+++ server/merger/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class PlainTransaction(models.Model):
+    id = models.AutoField(primary_key=True)
     date = models.DateField()
     description = models.CharField(max_length=255)
     account = models.CharField(max_length=255)
