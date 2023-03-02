290de84e0f465e0958f7704e11e70fc3aba2e57f
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-03-02 18:53:13 +0100

    Connect transactions endpoint to table

--- client/src/components/EntryTable/EntryTable.tsx
+++ client/src/components/EntryTable/EntryTable.tsx
@@ -2,44 +2,19 @@ import React from "react";
 import {Table} from "antd";
 import {Transaction} from "../../models/merger";
 
-const dataSource = [
-    {
-        key: '1',
-        name: 'Mike',
-        age: 32,
-        address: '10 Downing Street',
-    },
-    {
-        key: '2',
-        name: 'John',
-        age: 42,
-        address: '10 Downing Street',
-    },
-];
-
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-];
+const columns = ['id', 'date', 'description', 'account', 'category', 'amount'].map(c => ({
+    title: c,
+    dataIndex: c,
+    key: c
+}));
 
 type EntryTableProps = {
     data: Transaction[];
 }
 
 export const EntryTable = ({data}: EntryTableProps) => {
-    console.log(data)
+
+    const dataSource = data.map(d => ({...d, key: d.id}));
+
     return <Table dataSource={dataSource} columns={columns} />;
 }
