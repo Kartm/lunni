c8a1ad3695cc7533b24a2eceb97953f156974e9a
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-03-14 18:16:51 +0100

    Add loading indicator to table

--- client/src/pages/merger/MergerPage.tsx
+++ client/src/pages/merger/MergerPage.tsx
@@ -1,5 +1,5 @@
 import {EntryTable} from "../../components/EntryTable";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {FileUpload} from "../../components/FileUpload/FileUpload";
 import {useUploadFile} from "../../hooks/merger/useUploadFile";
 import {useGetTransactions} from "../../hooks/merger/useGetTransactions";
@@ -8,8 +8,8 @@ import {Key} from "antd/es/table/interface";
 import {useMergeMutations} from "../../hooks/merger/useMergeTransactions";
 
 export const MergerPage = () => {
-    const {isLoading, mutate} = useUploadFile();
-    const {data} = useGetTransactions();
+    const {isLoading: isUploadFileLoading, mutate} = useUploadFile();
+    const {data, isLoading: isGetTransactionsLoading} = useGetTransactions();
     const [mergeSelection, setMergeSelection] = useState<Key[]>([])
     const mergeTransactions = useMergeMutations();
 
@@ -21,10 +21,14 @@ export const MergerPage = () => {
         })
     }
 
+    useEffect(() => {
+        mergeTransactions.isSuccess && setMergeSelection([]);
+    }, [mergeTransactions.isSuccess])
+
     return <div>
-        <FileUpload isUploading={isLoading} onFileUpload={fileUpload => mutate(fileUpload)}/>
+        <FileUpload isUploading={isUploadFileLoading} onFileUpload={fileUpload => mutate(fileUpload)}/>
         <MergeButton mergeSelection={mergeSelection} data={data?.transactions || []} onMerge={handleMerge}/>
-        <EntryTable isLoading={isLoading} data={data?.transactions || []} onMergeSelectionChange={setMergeSelection}/>
+        <EntryTable isLoading={isGetTransactionsLoading || isUploadFileLoading} data={data?.transactions || []} onMergeSelectionChange={setMergeSelection}/>
     </div>
 
 }
