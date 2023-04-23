90690f54b4dc7de3fc64989fac647ca0cc8230ca
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-04-23 13:23:13 +0200

    Fix formatter

--- client/src/components/MergeButton/MergeButton.tsx
+++ client/src/components/MergeButton/MergeButton.tsx
@@ -46,14 +46,27 @@ export const MergeButton = ({
     onMerge(source!.id, target!.id, value!);
   };
 
-  const numberFormatter = (value?: number) =>
-    `PLN ${(parseInt(value?.toString() || "0", 10) / 100).toFixed(2)}`.replace(
-      /\B(?=(\d{3})+(?!\d))/g,
-      ","
-    );
+  // const numberFormatter = (value?: number) => {
+  //   const val = parseInt(value?.toString() || "0", 10);
+  //
+  //   console.log(val);
+  //   // `${(parseInt(value?.toString() || "0", 10) / 100).toFixed(2)}`;
+  //
+  //   return val > 100 ? (val / 100).toFixed(2) : (val / 100).toString(10);
+  // };
+  // const numberParser = (value?: string) => parseInt(value ?? "", 10) * 100;
 
-  const numberParser = (value?: string) =>
-    parseInt(value!.replace(/\$\s?|(,*)/g, ""), 10) * 100;
+  function formatMoney(value?: number) {
+    if (!value) return "";
+    return (value / 100).toFixed(2);
+  }
+
+  function parseMoney(value?: string) {
+    if (!value) return 0;
+    const numericValue = parseFloat(value);
+    const intValue = Math.round(numericValue * 100);
+    return isNaN(intValue) ? 0 : intValue;
+  }
 
   return (
     <Space.Compact>
@@ -64,8 +77,9 @@ export const MergeButton = ({
         min={0}
         max={source?.amount}
         onChange={setValue}
-        formatter={numberFormatter}
-        parser={numberParser}
+        formatter={formatMoney}
+        parser={parseMoney}
+        size="large"
       />
 
       <Button
