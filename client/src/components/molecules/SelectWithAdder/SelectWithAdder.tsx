3520841d9a3c730d554060348d35e8748b9e7b76
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-05-17 17:49:32 +0200

    Make SelectAdder a separate component

--- client/src/components/molecules/SelectWithAdder/SelectWithAdder.tsx
+++ client/src/components/molecules/SelectWithAdder/SelectWithAdder.tsx
@@ -1,29 +1,8 @@
-import {
-  Button,
-  Col,
-  Divider,
-  Drawer,
-  Form,
-  Input,
-  InputRef,
-  Row,
-  Select,
-  Space,
-  Tag,
-  Typography,
-} from "antd";
-import React, { useEffect, useRef, useState } from "react";
-import { useRematchCategories } from "../../../hooks/api/useRematchCategories";
-import { useCategoryStats } from "../../../hooks/api/useCategoryStats";
-import { SyncOutlined } from "@ant-design/icons";
-import { DataType } from "../../organisms/EntryTable";
-import TextArea from "antd/es/input/TextArea";
-import { PlusOutlined } from "@ant-design/icons";
+import { Divider, Select } from "antd";
+import React, { useState } from "react";
 import { SelectProps } from "antd/es/select";
 import { TransactionCategory } from "../../../models/merger";
-
-const { Text } = Typography;
-const { Option } = Select;
+import { SelectAdder } from "../../atoms/SelectAdder";
 
 type SelectWithAdderProps = {
   onAddOption: (name: string, variant: TransactionCategory["variant"]) => void;
@@ -35,34 +14,11 @@ export const SelectWithAdder = ({
 }: SelectWithAdderProps) => {
   const [open, setOpen] = useState(false);
 
-  const [newOption, setNewOption] = useState({
-    name: "",
-    variant: "NEG" as TransactionCategory["variant"],
-  });
-  const inputRef = useRef<InputRef>(null);
-
-  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewOption((old) => ({ ...old, name: event.target.value }));
-  };
-
-  const onVariantChange = (variant: TransactionCategory["variant"]) => {
-    setNewOption((old) => ({
-      ...old,
-      variant,
-    }));
-  };
-
-  const addItem = (
-    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>
+  const handleAddOption = (
+    name: string,
+    variant: TransactionCategory["variant"]
   ) => {
-    e.preventDefault();
-    onAddOption(newOption.name, newOption.variant);
-    setNewOption({ name: "", variant: "NEG" });
-
-    setTimeout(() => {
-      inputRef.current?.focus();
-    }, 0);
-
+    onAddOption(name, variant);
     setOpen(false);
   };
 
@@ -75,27 +31,7 @@ export const SelectWithAdder = ({
         <>
           {menu}
           <Divider style={{ margin: "8px 0" }} />
-          <Space style={{ padding: "0 8px 4px" }}>
-            <Input
-              placeholder="Category name"
-              ref={inputRef}
-              value={newOption.name}
-              onChange={onNameChange}
-            />
-
-            <Select
-              placeholder="Variant"
-              options={["NEG", "POS", "IGN"].map((o) => ({
-                label: o,
-                value: o,
-              }))}
-              onChange={onVariantChange}
-              onMouseDown={(e) => e.stopPropagation()} // prevent parent Select from closing
-            />
-          </Space>
-          <Button type="text" icon={<PlusOutlined />} onClick={addItem}>
-            Add item
-          </Button>
+          <SelectAdder onAddOption={handleAddOption} />
         </>
       )}
       {...props}
