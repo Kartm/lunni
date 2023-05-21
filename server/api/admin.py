0d4d8c1c8790113ade70f6cea17c36805b9269cf
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-05-21 16:27:38 +0200

    Fix django admin of Transaction

--- server/api/admin.py
+++ server/api/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from api.models import CategoryMatcher, Category, Transaction, TransactionMerge
 
 
-class TransactionLogAdmin(admin.ModelAdmin):
+class TransactionAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        qs = qs.admin_manager.all()
+        qs = qs.admin_objects.all()
         return qs
 
 
 admin.site.register(Category)
 admin.site.register(CategoryMatcher)
-admin.site.register(Transaction, TransactionLogAdmin)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(TransactionMerge)
