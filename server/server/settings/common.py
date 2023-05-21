2669b07c45ffa2385fc9d3df440bf81664cc692a
Lukasz Blachnicki <lblachnicki@gmail.com>
2023-05-21 17:06:02 +0200

    Make db volume persistent

--- server/server/settings/common.py
+++ server/server/settings/common.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
