10d98a40a8c8ec4c91c297cef4023497dcbf8ee0
Bartosz Kępka <bartoszkepka1999@gmail.com>
2023-07-03 00:15:11 +0200

    Add analysis using chartJS library

--- client/src/components/pages/analysis/AnalysisPage.tsx
+++ client/src/components/pages/analysis/AnalysisPage.tsx
@@ -1,21 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useUploadFileToAnalyse } from "../../../hooks/api/useUploadFileToAnalyse";
+import { Chart, registerables } from 'chart.js'
+import { Bar } from "react-chartjs-2";
+
 
 export const AnalysisPage = () => {
-  const {data , mutate} = useUploadFileToAnalyse()
-  
+  const { data, mutate } = useUploadFileToAnalyse()
+  const [options, setOptions] = useState({});
+  const [graphData, setGraphData] = useState<any>({
+    datasets: []
+  });
+
+  Chart.register(...registerables)
+
   useEffect(() => {
     mutate()
   }, [])
 
   useEffect(() => {
-    data && console.log(data)
+    data && prepareGraph(data)
   }, [data])
-  
-  
+
+  const prepareGraph = (jsonData: any) => {
+    const categories = [...new Set(jsonData.map((item: any) => item.category_name))];
+    const months = [...new Set(jsonData.map((item: any) => item.month))];
+
+    const labels = months;
+
+    const datasets = categories.map(category => {
+      const categoryData = jsonData
+        .filter((item: any) => item.category_name === category)
+        .map((item: any) => item.calculated_amount);
+
+      const randomColor = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+        Math.random() * 256
+      )}, ${Math.floor(Math.random() * 256)}, 0.5)`;
+
+      return {
+        label: category,
+        data: categoryData,
+        backgroundColor: randomColor,
+      };
+    });
+
+    const options = {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Your expenses per month',
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    };
+
+    const data = {
+      labels: labels,
+      datasets: datasets,
+    };
+
+    setOptions(options)
+    setGraphData(data)
+  }
+
   return (
     <>
-
+      {graphData && options && <Bar options={options} data={graphData} />};
     </>
   );
 };
